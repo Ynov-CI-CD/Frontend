c800467d4fe1c776bcb1c6b46fb288ed1387ea9f
Strackz <strackzdesign@gmail.com>
2025-01-17 15:18:22 +0100

    feat: api url in development mode

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   name: 'Development',
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://backend.integration-deploiement.strackzdev.com/'
 };
